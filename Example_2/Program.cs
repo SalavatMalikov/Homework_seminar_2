6360c76f7b9604b422cc93f90525a767b544a587
Salavat <malikov.salavat702@gmail.com>
2022-12-17 01:38:36 +0300

    2

---
+++ Example_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int num = new Random().Next(1, 100000);
+Console.WriteLine(num);
+
+string aNum = Convert.ToString(num);
+
+if (aNum.Length>2)
+{
+    Console.WriteLine(aNum[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
